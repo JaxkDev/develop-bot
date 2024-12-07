6f64192fe45860f5a07408b6d9ae6d0ef083ab19
Jack Honour <JaxkDev@gmail.com>
2024-12-07 13:41:12 +0000

    Production

--- src/jobs/api/faction.js
+++ src/jobs/api/faction.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 const logger = require('../../logger');
 const fs = require('fs');
 const { Client } = require('discord.js');
-const { log } = require('console');
 
 const faction_selections = [
 	'basic',
@@ -38,10 +37,16 @@ const faction_selections = [
 	'reports'
 ]
 
-// Run this every 2s
+const url = `${getConfig().api}/faction?key=${process.env.TORN_KEY}&selections=` + faction_selections.join(',');
+
+let cache = {
+	"attacks": []
+};
+
+// Run this every 3s
 module.exports = {
 	name: 'ApiFaction',
-    cron: '*/2 * * * * *',
+    cron: '*/3 * * * * *',
 
 	/**
 	 * @param {Client} client 
@@ -51,9 +56,7 @@ module.exports = {
 		if (!fs.existsSync('data/api/faction')) fs.mkdirSync('data/api/faction', { recursive: true });
 		const meta = JSON.parse(fs.existsSync('data/api/faction/meta.dat') ? fs.readFileSync('data/api/faction/meta.dat', 'utf8') : '{}');
 		const lastTimestamp = meta.timestamp || 0;
-
-		const url = `${getConfig().api}/faction?key=${process.env.TORN_KEY}&from=${lastTimestamp+1}&selections=` + faction_selections.join(',');
-		axios.get(url)
+		axios.get(url + `&from=${lastTimestamp}`)
 		.then(res => {
 			if(res.status !== 200) {
 				throw new Error('Failed to fetch data from Torn API');
@@ -67,12 +70,24 @@ module.exports = {
 			const attacks = data.attacks ?? [];
 			for (const attack of attacks.reverse()) {
 				const attackId = attack.id;
+				if (cache.attacks.includes(attackId)) {
+					logger.debug(`Faction attack data for attack ID ${attackId} is already processed.`);
+					continue;
+				}
+				// Check if the attack is already processed
 				client.emit('torn-attack', attack);
 				logger.debug(`Faction attack data emitted for attack ID ${attackId}`);
 				//logger.debug(`Faction attack data emitted for attack ID ${attackId}`);
-				fs.writeFileSync(`data/api/faction/attacks/${attack.ended}-${attackId}.dat`, JSON.stringify(attack));
+				//fs.writeFileSync(`data/api/faction/attacks/${attack.ended}-${attackId}.dat`, JSON.stringify(attack));
 				//logger.debug(`Faction attack data saved for attack ID ${attackId} to ${`data/api/faction/attacks-${attackId}.dat`}`);
 			}
+
+			if (attacks.length !== 0) {
+				//fs.writeFileSync(`data/api/faction/raw/${timestamp}.json`, JSON.stringify(data));
+			}
+
+			// replace cache with new data
+			cache.attacks = attacks.map(attack => attack.id);
 		})
 		.catch(err => {
 			logger.error("Failed to fetch faction data from Torn API", {error: err});
