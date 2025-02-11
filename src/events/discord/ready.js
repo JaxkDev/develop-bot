d1d0517a36e5a14beb93f70f5d66b2fcbf235637
Jack Honour <JaxkDev@gmail.com>
2025-02-11 09:06:00 +0000

    Disable report gen for last

--- src/events/discord/ready.js
+++ src/events/discord/ready.js
@@ -37,19 +37,19 @@ module.exports = {
         }
         logger.debug('Jobs scheduled with scheduler.');
 
-        let twar = {
-            factions: [
-              {id: 8869, name: 'Bendetti', score: 4393, chain: 0 },
-              {id: 51716, name: 'Develop (Hiring)', score: 3896, chain: 0 }
-            ],
-            start: 1738861200, end: 1739257200, target: 497, winner: 8869,
-            war_id: 22005
-        }
-        try{
-            generateAllReports(client, twar, true);
-        } catch(err) {
-            logger.error("Failed to gen report:", {'err': err.toString()})
-        }
-        console.log('done');
+        // let twar = {
+        //     factions: [
+        //       {id: 8869, name: 'Bendetti', score: 4393, chain: 0 },
+        //       {id: 51716, name: 'Develop (Hiring)', score: 3896, chain: 0 }
+        //     ],
+        //     start: 1738861200, end: 1739257200, target: 497, winner: 8869,
+        //     war_id: 22005
+        // }
+        // try{
+        //     generateAllReports(client, twar, true);
+        // } catch(err) {
+        //     logger.error("Failed to gen report:", {'err': err.toString()})
+        // }
+        // console.log('done');
 	},
 };
